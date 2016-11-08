2ef2f0c67b57be5afc883ce4568c31ac8a29ede3
wynandstolp <wynandstolp@gmail.com>
2016-11-08 20:11:29 +0200

    WIP

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
@@ -37,11 +42,21 @@ module SessionsHelper
     end
 
   # Forgets a persistent session.
-def forget(user)
-  user.forget
-  cookies.delete(:user_id)
-  cookies.delete(:remember_token)
-end
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 
 end
